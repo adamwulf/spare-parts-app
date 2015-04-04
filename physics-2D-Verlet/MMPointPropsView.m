6470227a88087a5e0e84944e8d22e21548a6f9b3
Adam Wulf <adam.wulf@gmail.com>
2015-04-04 14:47:10 -0500

    point props are done

--- physics-2D-Verlet/MMPointPropsView.m
+++ physics-2D-Verlet/MMPointPropsView.m
@@ -14,6 +14,7 @@
     UISlider* gravitySlider;
     UISwitch* immovableSwitch;
     MMPoint* selectedPoint;
+    UILabel* gravityLbl;
 }
 
 -(id) initWithFrame:(CGRect)frame{
@@ -37,18 +38,41 @@
         [self addSubview:immovableSwitch];
         
         UILabel* lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 60)];
-        lbl.text = @"immovable";
+        lbl.text = @"Immovable";
         [lbl sizeToFit];
         lbl.center = CGPointMake(120, 60);
         [self addSubview:lbl];
         
-        gravitySlider = [[UISlider alloc] initWithFrame:CGRectMake(20, 110, self.bounds.size.width-40, 40)];
+
+        gravityLbl = [[UILabel alloc] initWithFrame:CGRectMake(20, 110, 100, 40)];
+        gravityLbl.text = @"Gravity: 0";
+        [gravityLbl sizeToFit];
+        CGRect fr = gravityLbl.frame;
+        fr.size.width = self.bounds.size.width;
+        gravityLbl.frame = fr;
+        [self addSubview:gravityLbl];
+        gravitySlider = [[UISlider alloc] initWithFrame:CGRectMake(30, 110 + lbl.bounds.size.height, self.bounds.size.width-60, 40)];
         gravitySlider.minimumValue = -2;
         gravitySlider.maximumValue = 2;
         gravitySlider.continuous = YES;
         [gravitySlider addTarget:self action:@selector(gravityChanged:) forControlEvents:UIControlEventValueChanged];
         [self addSubview:gravitySlider];
         
+        lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+        lbl.text = @"-2";
+        [lbl sizeToFit];
+        [self addSubview:lbl];
+        lbl.textAlignment = NSTextAlignmentCenter;
+        lbl.center = CGPointMake(15, gravitySlider.center.y);
+        
+        
+        lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+        lbl.text = @"+2";
+        [lbl sizeToFit];
+        [self addSubview:lbl];
+        lbl.textAlignment = NSTextAlignmentCenter;
+        lbl.center = CGPointMake(self.bounds.size.width - 15, gravitySlider.center.y);
+
         UIView* bitsOfWhite = [[UIView alloc] initWithFrame:self.bounds];
         bitsOfWhite.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:.5];
         UIBlurEffect * blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
@@ -65,6 +89,7 @@
     selectedPoint = point;
     immovableSwitch.on = point.immovable;
     gravitySlider.value = point.gravityModifier;
+    gravityLbl.text = [NSString stringWithFormat:@"Gravity: %.1f", point.gravityModifier];
 }
 
 -(void) immovableChanged{
@@ -73,6 +98,7 @@
 
 -(void) gravityChanged:(UISlider*)_slider{
     selectedPoint.gravityModifier = _slider.value;
+    gravityLbl.text = [NSString stringWithFormat:@"Gravity: %.1f", selectedPoint.gravityModifier];
 }
 
 @end
